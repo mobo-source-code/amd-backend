d9ffd253c24c1c972d96b72c90833b4b9e56f96c
Mohammed Bouaziz <mobo.repos@gmail.com>
2022-09-12 14:49:51 +0100

    prod

--- contact/settings.py
+++ contact/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = 'django-insecure-3u%eq@kxjfb!7*91(ew$%9eh-7u28r1r!90qh=v8&8=mnj1pb)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*',]
 
 
 # Application definition
@@ -95,6 +95,9 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
@@ -113,7 +116,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-CORS_ALLOW_ALL_ORIGINS: True
+CORS_ALLOW_ALL_ORIGINS = True
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
